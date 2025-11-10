639ee4e7099548a6fb6a020b35a6b7e15f0c6397
Rishabh Singh <rishabhsvats@gmail.com>
2025-11-10 11:01:38 +0000

    check 5 from devspaces

--- main.go
+++ main.go
@@ -17,5 +17,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World! from Rishabh! Check4 - from devspaces")
+	fmt.Fprint(w, "Hello World! from Rishabh! Check5 - from devspaces")
 }
