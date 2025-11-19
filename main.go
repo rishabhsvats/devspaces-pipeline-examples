a0fbb687cb0da757ba1b05ea5cde5dd0053433a1
Rishabh Singh <rishabhsvats@gmail.com>
2025-11-19 17:23:19 +0530

    new pipeline instance

--- main.go
+++ main.go
@@ -17,5 +17,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World! from Rishabh! Check7 on new - from devspaces")
+	fmt.Fprint(w, "Hello World! from Rishabh! Check8 on new - from devspaces")
 }
