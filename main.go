572ee941386f4fcb9847278d178a23987b215516
Rishabh Singh <rishabhsvats@gmail.com>
2026-01-12 13:25:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -17,5 +17,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World! from Rishabh! Check10 on new - from devspaces")
+	fmt.Fprint(w, "Hello World! from Rishabh! Check on new - from devspaces")
 }
